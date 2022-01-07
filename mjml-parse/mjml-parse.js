4cb2cf4f046952e901fd70952ea5c91e7867591c
Andre Lademann <andre.lademann@netresearch.de>
2022-01-08 00:01:50 +0100

    #8 Add german translations for JavaScript

--- mjml-parse/mjml-parse.js
+++ mjml-parse/mjml-parse.js
@@ -11,9 +11,9 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         var node = this;
         node.on('input', function (msg) {
+            let msgError = RED._("mjmlParse.message.status.error");
             // Check if the message is a string
             if (typeof msg.payload !== "string") {
-                let msgError = 'The message is not a string';
                 node.status({fill: "red", shape: "ring", text: msgError})
                 // Send the error
                 node.error(msgError);
@@ -24,19 +24,19 @@ module.exports = function (RED) {
                 const result = mjml2html(msg.payload, options);
                 // Check if the result is valid
                 if (result.errors.length > 0) {
-                    node.status({fill: "red", shape: "ring", text: "error"})
+                    node.status({fill: "red", shape: "ring", text: "node-red:common.status.error"})
                     // Send the error
                     node.error(result.errors);
                 } else {
-                    node.status({fill: "green", shape: "ring", text: "ok"})
+                    node.status({fill: "green", shape: "ring", text: "node-red:common.status.ok"})
                     // Send the result
                     msg.payload = result.html;
                     node.send(msg);
                 }
             } catch (error) {
-                node.status({fill: "red", shape: "ring", text: error.message})
+                node.status({fill: "red", shape: "ring", text: msgError})
                 // Send the error
-                node.error(error);
+                node.error(error.message);
             }
         });
     }
